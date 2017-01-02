00c71217218f4544468f0da73589cbe160400b60
psnareshdhuliya <naresh.dhuliya@thepsi.com>
2017-01-02 13:00:09 +0530

    Added a employee class

---
+++ Git-VSExample/Class/Demo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Git_VSExample.Class
+{
+    class Employee
+    {
+        public int EmployeeID { get; set; }
+        public string Name { get; set; }
+    }
+}
